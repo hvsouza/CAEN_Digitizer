4c0a956e0376b1ec627faadc0b946bc4415f00f1
Henrique Souza <henriquevieira.souza@gmail.com>
2022-09-24 01:53:47 +0200

    Recompile also the factor number..

--- recompile_wavedump.sh
+++ recompile_wavedump.sh
@@ -6,28 +6,35 @@ else
     npts=$1
 fi
 
+if [ -z $2 ]; then
+    factor=2
+else
+    factor=$2
+fi
+
 ROOTPATH=~/Documents/CAEN_Digitizer
-SOURCEFILES=installation_files
 CURRENT=$( pwd )
 
-
-cd $ROOTPATH/$SOURCEFILES
-linenumber=$(eval "sed -n '/uint64_t mymaximum/=' WaveDump.c") # search line with pathern
-sed -i "$linenumber d" WaveDump.c
-sed -i "$linenumber i \ \ \ \ uint64_t mymaximum = $npts; \/\/ Added by Henrique Souza" WaveDump.c
-
-
-sleeptime=3
-echo "Recompiling wavedump with a maximum of $npts waveforms"
-echo ""
-sleep $sleeptime
 
 cd $ROOTPATH
 cd $( /bin/ls | grep wavedump* )
 cd src
-#replacing the .c file with my custom one
-cp $ROOTPATH/$SOURCEFILES/WaveDump.c .
+linenumber=$(eval "sed -n '/uint64_t mymaximum/=' WaveDump.c") # search line with pathern
+sed -i "$linenumber d" WaveDump.c
+sed -i "$linenumber i \ \ \ \ uint64_t mymaximum = $npts; \/\/ Added by Henrique Souza" WaveDump.c
+
+linenumber_factor=$(eval "sed -n '/int factor/=' WaveDump.c") # search line with pathern
+sed -i "$linenumber_factor d" WaveDump.c
+sed -i "$linenumber_factor i \ \ \ \ \ \ \ \ \ \ \ \ int factor = $factor; \/\/ Added by Henrique Souza" WaveDump.c
+
+
+
+sleeptime=3
+echo "Recompiling wavedump with a maximum of $npts waveforms and factor of $factor"
+echo ""
+sleep $sleeptime
 cd ..
+
 
 ./configure
 sudo make
