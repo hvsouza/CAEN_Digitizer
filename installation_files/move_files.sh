43e249f5a7b5091a708a44b2344b036f7d230cf3
Henrique Souza <henriquevieira.souza@gmail.com>
2022-10-07 13:15:56 +0200

    GUI with python release!

--- installation_files/move_files.sh
+++ installation_files/move_files.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-~/Documents/CAEN_Digitizer/move_files/build-move_files-Desktop_Qt_5_GCC_64bit-Release/move_files &
-# ~/Documents/QtCreator/build-move_files-Desktop_Qt_6_2_4_GCC_64bit-Release/move_files &
+python3 ~/Documents/CAEN_Digitizer/pythonQt/move_files.py &
