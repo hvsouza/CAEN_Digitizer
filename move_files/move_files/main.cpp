9f5c099c29cedc12312be9fd6cf4b414a28c4093
Henrique Souza <henriquevieira.souza@gmail.com>
2022-11-02 22:02:44 +0100

    Removing some buttons... C++ version not functional any more :sad face:

--- move_files/move_files/main.cpp
+++ move_files/move_files/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    w.disabletriggers();
-    w.setRate();
+//    w.disabletriggers();
+//    w.setRate();
 
     return a.exec();
 }
--- move_files/move_files/mainwindow.h
+++ move_files/move_files/mainwindow.h
@@ -18,71 +18,71 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    void disabletriggers();
+//    void disabletriggers();
     void setRate();
 
     ~MainWindow();
 
 private:
-    bool move_data_file(int run, int subrun, double voltage, double threshold, std::string triggerCh, std::string extra, std::string primary, std::string file_type);
-    bool move_data_file_style2(int run, int subrun, std::string block1, std::string block2, std::string extra, std::string primary, std::string file_type);
+//    bool move_data_file(int run, int subrun, double voltage, double threshold, std::string triggerCh, std::string extra, std::string primary, std::string file_type);
+//    bool move_data_file_style2(int run, int subrun, std::string block1, std::string block2, std::string extra, std::string primary, std::string file_type);
 
-    bool move_calibration_file(int run, int subrun, double voltage, double threshold, std::string triggerCh, std::string extra, std::string primary, std::string file_type);
+//    bool move_calibration_file(int run, int subrun, double voltage, double threshold, std::string triggerCh, std::string extra, std::string primary, std::string file_type);
 
-    std::string folder_name();
-    std::string folder_name2();
+//    std::string folder_name();
+//    std::string folder_name2();
 
-    std::string changeVoltage(double voltage);
-    bool checkSpaces(std::string var);
-    int getFactor();
-    void writeConfigFile(bool extra);
-    void save_config_file(std::string folder);
-    bool transfer_files(std::vector<std::string> mvi);
+//    std::string changeVoltage(double voltage);
+//    bool checkSpaces(std::string var);
+//    int getFactor();
+//    void writeConfigFile(bool extra);
+//    void save_config_file(std::string folder);
+//    bool transfer_files(std::vector<std::string> mvi);
 
 
 
 private slots:
-    void on_pushButton_2_clicked();
-    void on_pushButton_4_clicked();
+//    void on_pushButton_2_clicked();
+//    void on_pushButton_4_clicked();
 
-    void on_button_movefile_clicked();
-    void on_button_movefile_5_clicked();
+//    void on_button_movefile_clicked();
+//    void on_button_movefile_5_clicked();
 
-    void on_button_movefile_2_clicked();
+//    void on_button_movefile_2_clicked();
 
-    //void on_radioButton_2_clicked(bool checked);
+//    //void on_radioButton_2_clicked(bool checked);
 
-    void on_pushButtonRecompile_clicked();
+//    void on_pushButtonRecompile_clicked();
 
-    void on_lock_folder_clicked(bool checked);
+//    void on_lock_folder_clicked(bool checked);
 
-    void enable_and_trigger(bool checked, QCheckBox* myt);
-    void setEnabledTrigger(QCheckBox *myt);
-    void setDisabledTrigger(QCheckBox *myt);
-    void on_externaltrigger_clicked(bool checked);
-
-
-
-    void on_enable1_clicked(bool checked);
-    void on_enable2_clicked(bool checked);
-    void on_enable3_clicked(bool checked);
-    void on_enable4_clicked(bool checked);
-    void on_enable5_clicked(bool checked);
-    void on_enable6_clicked(bool checked);
-    void on_enable7_clicked(bool checked);
-    void on_enable8_clicked(bool checked);
+//    void enable_and_trigger(bool checked, QCheckBox* myt);
+//    void setEnabledTrigger(QCheckBox *myt);
+//    void setDisabledTrigger(QCheckBox *myt);
+//    void on_externaltrigger_clicked(bool checked);
 
 
 
-    void on_FileTypeSet_currentTextChanged(const QString &arg1);
+//    void on_enable1_clicked(bool checked);
+//    void on_enable2_clicked(bool checked);
+//    void on_enable3_clicked(bool checked);
+//    void on_enable4_clicked(bool checked);
+//    void on_enable5_clicked(bool checked);
+//    void on_enable6_clicked(bool checked);
+//    void on_enable7_clicked(bool checked);
+//    void on_enable8_clicked(bool checked);
 
-    void on_pushButton_SetConfig_clicked();
 
-    void on_button_save_config_2_clicked();
 
-    void on_button_save_config_clicked();
+//    void on_FileTypeSet_currentTextChanged(const QString &arg1);
 
-    void on_samplingRate_2_currentTextChanged(const QString &arg1);
+//    void on_pushButton_SetConfig_clicked();
+
+//    void on_button_save_config_2_clicked();
+
+//    void on_button_save_config_clicked();
+
+//    void on_samplingRate_2_currentTextChanged(const QString &arg1);
 
 private:
     Ui::MainWindow *ui;
